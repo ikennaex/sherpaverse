c669205754a93dbe96d955a712bbe3f23dd2821a
Excel Ikenna <ikennaexcel@gmail.com>
2025-02-10 10:14:49 +0100

    new stuff

--- src/Pages/Homepage/Blog.jsx
+++ src/Pages/Homepage/Blog.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Blog = () => {
   return (
     <div className='text-xl font-bold text-center'>
-      Blog is under contruction
+      Blog is under construction
     </div>
   )
 }
